980a03d2834a0fa84ec81c87fa89a06759e808e0
Demo-1 <pankajbadgujar799@gmail.com>
2024-02-06 22:39:06 +0530

    new files added

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+
+from . import views
+from django.urls import path,include
+
+urlpatterns = [
+    path('',views.home , name='home'),
+   # path('login/',views.login_user , name='login'),
+   # path('logout/',views.logout_user , name='logout'),
+
+]
